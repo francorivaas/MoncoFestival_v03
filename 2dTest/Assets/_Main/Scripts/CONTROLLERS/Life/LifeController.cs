ed42422fe37bfc6ee3de43aaefeadabc5b17c0f9
Franco Rivas <francorivaa19@gmail.com>
2020-12-14 14:28:28 -0300

    borro SystemSecurityCriptography

--- 2dTest/Assets/_Main/Scripts/CONTROLLERS/Life/LifeController.cs
+++ 2dTest/Assets/_Main/Scripts/CONTROLLERS/Life/LifeController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
