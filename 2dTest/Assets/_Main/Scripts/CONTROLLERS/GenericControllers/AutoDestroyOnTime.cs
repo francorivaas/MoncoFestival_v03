9bd3e5118ec1b6b43e73767c5a8cabdbd196b86b
Franco Rivas <francorivaa19@gmail.com>
2020-12-17 12:24:15 -0300

    TERMINAR DEACTIVATOR

--- 2dTest/Assets/_Main/Scripts/CONTROLLERS/GenericControllers/AutoDestroyOnTime.cs
+++ 2dTest/Assets/_Main/Scripts/CONTROLLERS/GenericControllers/AutoDestroyOnTime.cs
@@ -10,6 +10,13 @@ public class AutoDestroyOnTime : MonoBehaviour
     [SerializeField]
     private float currentLife = 0.0f;
 
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         currentLife += Time.deltaTime;
@@ -18,5 +25,11 @@ public class AutoDestroyOnTime : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Colliders")) animator.SetTrigger("Destroy");
+
     }
 }
