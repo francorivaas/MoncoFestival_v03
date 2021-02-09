625ddad8bc38b3fdf7ad3b6c409a1d9a3eb9e412
Franco Rivas <francorivaa19@gmail.com>
2021-02-09 10:58:35 -0300

    animación de título

---
+++ 2dTest/Assets/_Main/Scripts/CONTROLLERS/Managers/EnemiesKilledCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+public class EnemiesKilledCounter : MonoBehaviour
+{
+    GameObject[] enemiesK;
+    public Text enemiesKText;
+
+    void Update()
+    {
+
+        enemiesKText.text = "Enemies" + enemiesK.Length.ToString();
+    }
+}
+*/
---
+++ 2dTest/Assets/_Main/Scripts/TitleAnimationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleAnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    private float timeToAnim = 1.0f;
+    private float cuirrentTimeToAnim = 0.0f;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        cuirrentTimeToAnim += Time.deltaTime;
+
+        if (cuirrentTimeToAnim >= timeToAnim)
+        {
+            animator.SetBool("Finished", true);
+        }
+    }
+}
